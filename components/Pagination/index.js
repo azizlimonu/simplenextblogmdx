b5b4c059caa58ee5e289d23ddf954571b5b7c839
Aziz Limonu <azizlimonu@gmail.com>
2023-06-19 15:26:53 +0800

    fix implement pagination

--- components/Pagination/index.js
+++ components/Pagination/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import { useRouter } from "next/router";
+import styles from './styles.module.scss';
+import Link from "next/link";
 
-const Pagination = () => {
+const Pagination = ({ totalPages, currentPage }) => {
+  const router = useRouter();
+
   return (
-    <div>Pagination</div>
+    <div className={styles.pagination}>
+      <p className={styles.pagination_heading}>
+        page {router.query.page || 1} out of {totalPages}
+      </p>
+
+      <div className={styles.paginateLinks_container}>
+        {currentPage > 1 && (
+          <Link href={`/?page=${currentPage - 1}`}>
+            <a className={styles.paginateLinks}>Prev</a>
+          </Link>
+        )}
+        {currentPage < totalPages && (
+          <Link href={`/?page=${currentPage + 1}`}>
+            <a className={styles.paginateLinks}>Next</a>
+          </Link>
+        )}
+      </div>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,23 @@ import matter from 'gray-matter';
 import fs from 'fs';
 import path from 'path';
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 import styles from '../styles/Home.module.css'
 import HeroSection from '../components/Home/Index'
 import { postFileNames, postsPath } from '../utils/mdxUtils'
 import BlogList from '../components/Blog/BlogList'
 import TagFilter from '../components/Blog/TagFilter'
+import Pagination from '../components/Pagination';
 
 export default function Home({ posts }) {
 
   const [selectedTag, setSelectedTag] = useState("all");
   const [filteredPost, setFilteredPost] = useState(posts);
+
+  const [currentPage, setCurrentPage] = useState(null);
+  const postPerPage = 3;
+  const router = useRouter();
 
   // Get All Tags 
   const allTagsSet = posts.reduce((acc, post) => {
@@ -22,18 +28,38 @@ export default function Home({ posts }) {
     return acc;
   }, new Set([]));
 
-  // // convert Tag Set To Array
+  // convert Tag Set To Array
   const allTags = [...allTagsSet].sort((a, b) => a.localeCompare(b));
   allTags.unshift("all");
 
-  // filter the post according to the tag
+  // filter the post according to the tag and pagination
   useEffect(() => {
-    const filtered = posts.filter(
-      post => post.frontmatter.tags.includes(selectedTag) || selectedTag === 'all'
-    );
+    let tempPosts = [...posts];
 
-    setFilteredPost(filtered);
-  }, [posts, selectedTag]);
+    if (selectedTag && selectedTag !== "all") {
+      tempPosts = posts.filter((post) =>
+        post.frontmatter.tags.includes(selectedTag)
+      );
+    }
+
+    const page = parseInt(router.query.page, 10) || 1;
+    setCurrentPage(page);
+
+    const startPage = (page - 1) * postPerPage;
+    const endPage =
+      startPage + postPerPage > posts.length - 1
+        ? posts.length
+        : startPage + postPerPage;
+
+    const paginatedPosts = tempPosts.slice(startPage, endPage);
+    setFilteredPost(paginatedPosts);
+
+  }, [posts, router, selectedTag]);
+
+  const totalPages =
+    selectedTag === 'all'
+      ? Math.ceil(posts.length / postPerPage)
+      : Math.ceil(filteredPost.length / postPerPage);
 
   return (
     <>
@@ -50,7 +76,13 @@ export default function Home({ posts }) {
         tags={allTags}
       />
       <BlogList posts={filteredPost} />
+
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+      />
     </>
+
 
   )
 };
