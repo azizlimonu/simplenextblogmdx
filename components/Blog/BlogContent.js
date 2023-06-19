42554531511562dff1016155fbc088d6e0c613b2
Aziz Limonu <azizlimonu@gmail.com>
2023-06-19 15:08:22 +0800

    updated blog content

--- components/Blog/BlogContent.js
+++ components/Blog/BlogContent.js
@@ -1,8 +1,31 @@
 import styles from './blogcontent.module.scss';
+import { MDXRemote } from 'next-mdx-remote';
+import Text from '../Typography/Text';
+import Heading from '../Typography/Heading';
+import Image from 'next/image';
 
-const BlogContent = () => {
+const content = {
+  p: (props) => <Text {...props} />,
+  h1: (props) => <Heading {...props} />,
+  h2: (props) => <Heading {...props} level={2} />,
+  h3: (props) => <Heading {...props} level={3} />,
+  img: (props) => (
+    <span className={styles.blogImage}>
+      <Image
+        {...props}
+        layout="fill"
+        alt={props.alt}
+        objectFit="cover"
+      />
+    </span>
+  ),
+}
+
+const BlogContent = ({ MdxSource }) => {
   return (
-    <div>BlogContent</div>
+    <div className={styles.container}>
+      <MDXRemote {...MdxSource} components={content} />
+    </div>
   )
 }
 
